8f0edc5fd00fa128f3ce6f116083d342de71af4d
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2016-07-12 18:58:14 +0530

    Nimesh/Priya: Added edit and delete

--- app/views.py
+++ app/views.py
@@ -3,6 +3,11 @@ from app import app, db
 from app.models import Post
 
 
+@app.route('/' )
+def index():
+  post = Post.query.all()
+  return render_template('index.html', post=post)
+
 @app.route('/add', methods=['POST', 'GET'])
 def add():
     if request.method == 'POST':
@@ -12,3 +17,25 @@ def add():
         flash('New entry was successfully posted')
 
     return render_template('add.html')
+
+
+@app.route('/edit/<id>', methods=['POST', 'GET'])
+def edit(id):
+    # Getting user by primary key:
+    post = Post.query.get(id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.text = request.form['body']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', post=post)
+
+
+@app.route('/delete/<id>', methods=['POST', 'GET'])
+def delete(id):
+    post = Post.query.get(id)
+    db.session.delete(post)
+    db.session.commit()
+    flash('deleted')
+
+    return redirect(url_for('index'))
