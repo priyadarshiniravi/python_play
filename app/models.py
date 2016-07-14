3b60e90ce2af47fa1a19d44264e0ec2e0db5b2e6
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2016-07-14 17:21:48 +0530

    Nimesh/Priya : Added column date for post in DB.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from app import db
 
 
@@ -5,16 +7,17 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128))
     body = db.Column(db.Text)
+    date = db.Column(db.Date)
 
     def __init__(self, title, body):
         # type: (object, object) -> object
         self.title = title
         self.body = body
+        self.date = datetime.now().date()
 
     def __eq__(self, other):
         return self.title == other.title and self.body == other.body
 
     def __hash__(self):
-        print "========"
         return self.title.__hash__ + self.body.__hash__
 
